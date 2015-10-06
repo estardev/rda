d9eb62177ad4cab3513abb3cc9dfed583970549b
ilariagiannetti <ilariagiannetti@gmail.com>
2015-10-06 17:15:18 +0200

    primo commit ore 17

--- README.md
+++
@@ -1,4 +0,0 @@
-rdaProva
-========
-
-A Symfony project created on October 6, 2015, 12:53 pm.
--- src/estar/rda/RdaBundle/estarRdaBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace estar\rda\RdaBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class estarRdaBundle extends Bundle
-{
-}
