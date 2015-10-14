a932ab36d2cda7c5f3eab2f7e2eea776f1e59f1f
MrZANO <mrzano.spam@gmail.com>
2015-10-14 15:39:09 +0200

    Rejected commit: Default

--- src/estar/rda/RdaBundle/Controller/HomePageController.php
+++ src/estar/rda/RdaBundle/Controller/HomePageController.php
@@ -8,10 +8,24 @@ class HomePageController extends Controller
 {
     public function indexAction()
     {
+        $utente= $this->getUser();
+
+        $em = $this->getDoctrine()->getManager();
+
+        $richiesta = $em->getRepository('estarRdaBundle:Richiesta')->findAll();
+        $categoria = $em->getRepository('estarRdaBundle:Categoria')->findAll();
+
+        return $this->render('estarRdaBundle:HomePage:index.html.twig', array(
+            'richiesta' => $richiesta,
+            'categoria'=> $categoria,
+            'utente' => $utente,
+        ));
+
         //$mySESSION= var_dump($_SESSION['_sf2_attributes']['_security_main']);
         //$myBoolean = $this->authorizationChecker->isGranted('ROLE_NEWSLETTER_ADMIN');
         //file_put_contents("ciao.txt", print_r($_SESSION['_sf2_attributes']['_security_main'], true));
         //file_put_contents("ciao.txt", print_r($myBoolean, true));
-        return $this->render('estarRdaBundle:HomePage:index.html.twig'/*,array('sessione'=>$mySESSION)*/);
+        //return $this->render('estarRdaBundle:HomePage:index.html.twig'/*,array('sessione'=>$mySESSION)*/);
     }
+
 }
