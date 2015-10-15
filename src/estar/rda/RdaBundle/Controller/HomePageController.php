1c85b9cd008b5ccabdae3c51e089c9d42d8c266e
FrancescoGalliEstar <francesco01.galli@estar.toscana.it>
2015-10-15 17:54:47 +0200

    sistemato file di demetrio

--- src/estar/rda/RdaBundle/Controller/HomePageController.php
+++ src/estar/rda/RdaBundle/Controller/HomePageController.php
@@ -9,28 +9,28 @@ class HomePageController extends Controller
     public function indexAction()
     {
        $utenteSessione= $this->get('security.context')->getToken()->getUser();
-       $idutenteSessione = $utenteSessione->getId();
-
+       //$idutenteSessione = $utenteSessione->getId();
+       //
         $em = $this->getDoctrine()->getManager();
-        $repository = $this->getDoctrine()->getRepository('estarRdaBundle:Utente');
-        $utente = $repository->findOneBy(array(
-            'idfosuser'=>$idutenteSessione)
-        );
-        $idutente = $utente->getId();
-        $repository = $this->getDoctrine()->getRepository('estarRdaBundle:Utentegruppoutente');
-        $utentegruppo = $repository->findOneBy(array(
-                'idutente'=>$idutente)
-        );
-        $idutentegruppo = $utentegruppo->getIdgruppoutente()->getId();
-        $repository = $this->getDoctrine()->getRepository('estarRdaBundle:Categoriagruppo');
-        $campogruppo = $repository->findOneBy(array(
-                'idgruppoutente'=>$idutentegruppo)
-        );
-        $abilitatoinserimentorichieste = $campogruppo->getAbilitatoinserimentorichieste();
-        $validatoretecnico = $campogruppo->getValidatoretecnico();
-        $validatoreAmministrativo = $campogruppo->getValidatoreamministrativo();
-        dump($campogruppo);
-
+       // $repository = $this->getDoctrine()->getRepository('estarRdaBundle:Utente');
+       // $utente = $repository->findOneBy(array(
+       //     'idfosuser'=>$idutenteSessione)
+       // );
+       // $idutente = $utente->getId();
+       // $repository = $this->getDoctrine()->getRepository('estarRdaBundle:Utentegruppoutente');
+       // $utentegruppo = $repository->findOneBy(array(
+       //         'idutente'=>$idutente)
+       // );
+       // $idutentegruppo = $utentegruppo->getIdgruppoutente()->getId();
+       // $repository = $this->getDoctrine()->getRepository('estarRdaBundle:Categoriagruppo');
+       // $campogruppo = $repository->findOneBy(array(
+       //         'idgruppoutente'=>$idutentegruppo)
+       // );
+       // $abilitatoinserimentorichieste = $campogruppo->getAbilitatoinserimentorichieste();
+       // $validatoretecnico = $campogruppo->getValidatoretecnico();
+       // $validatoreAmministrativo = $campogruppo->getValidatoreamministrativo();
+       // dump($campogruppo);
+       //
         $richiesta = $em->getRepository('estarRdaBundle:Richiesta')->findAll();
         $categoria = $em->getRepository('estarRdaBundle:Categoria')->findAll();
 
