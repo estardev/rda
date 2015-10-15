dbdd66b3579b6bc20824fbbb8cdda7ff2d973ee3
nadietta <cecilietta84@gmail.com>
2015-10-15 11:51:58 +0200

    SelectCategoria su Homepage punta alla url corretta

--- web/js/homepage.js
+++ web/js/homepage.js
@@ -1,6 +1,7 @@
 $(document).on("click", "#homepageSelectBtnCategoria", function(){
 
     var homepageSelectCategoria = $('#homepageSelectCategoria').val();
+    //alert("Prova");
     //alert("CategoriaId: " + homepageSelectCategoria);
 
     var url = "formtemplate/"+homepageSelectCategoria+"/new";
