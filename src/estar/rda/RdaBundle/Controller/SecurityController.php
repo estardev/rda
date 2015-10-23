d80ce4692d59db16ac53ca375ddb6d086c19f934
David Frassi <info@davidfrassi.it>
2015-10-23 17:29:53 +0200

    Integrato il sistema di autenticazione tramite carta operatore e bypass dell'autenticazione nativa di Symfony

--- src/estar/rda/RdaBundle/Controller/SecurityController.php
+++ src/estar/rda/RdaBundle/Controller/SecurityController.php
@@ -9,14 +9,51 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\SecurityContextInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use FOS\UserBundle\Doctrine\UserManager;
+use Symfony\Component\Security\Core\Authentication\Token\AbstractToken;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+
+
+
 
 class SecurityController extends BaseController
 {
     public function loginAction(Request $request)
     {
         /** @var $session \Symfony\Component\HttpFoundation\Session\Session */
+
         $session = $request->getSession();
 
+        // chiamata del metodo di verifica stringa carta operatore
+
+        $uid = $this->checkSmartCardString($request);
+
+        // se $uid contiene false c'è stato un errore, altrimenti $uid contiene il CF
+
+        if ($uid!=false){
+
+
+            // si recupera l'id utente a seguito della query per CF
+
+            $repository = $this->getDoctrine()->getRepository('estarRdaBundle:Utente');
+            $utente = $repository->findOneBy(array(
+                    'utentecartaoperatore'=>$uid)
+            );
+            $username = $utente->getIdfosuser()->getUsername();
+
+            $userManager = $this->get('fos_user.user_manager');
+            $user = $userManager->findUserByUsername($username);
+
+            // metodo per bypassare l'autenticazione nativa di symfony
+
+            $token = new UsernamePasswordToken($user, null, 'main', $user->getRoles());
+            $this->get('security.token_storage')->setToken($token);
+
+            return $this->render('estarRdaBundle:HomePage:index.html.twig', array('utente' => $user));
+
+
+
+       }
         if (class_exists('\Symfony\Component\Security\Core\Security')) {
             $authErrorKey = Security::AUTHENTICATION_ERROR;
             $lastUsernameKey = Security::LAST_USERNAME;
@@ -52,10 +89,24 @@ class SecurityController extends BaseController
                 : null;
         }
 
+
+        // parametri per il redirect di ritorno da parte del server di autenticazione, nel caso ok o ko dalla verifica
+        // della carta operatore
+
+        $urlok = "http://127.0.0.1/rda/web/app_dev.php/login";
+        $urlko = "http://127.0.0.1/rda/web/app_dev.php/register";
+        $urlok = base64_encode($urlok);
+        $urlko = base64_encode($urlko);
+
+
         return $this->renderLogin(array(
             'last_username' => $lastUsername,
+
             'error' => $error,
             'csrf_token' => $csrfToken,
+
+            'urlok'=>$urlok,
+            'urlko'=>$urlko,
         ));
 
     }
@@ -70,11 +121,15 @@ class SecurityController extends BaseController
      */
     protected function renderLogin(array $data)
     {
+
         return $this->render('FOSUserBundle:Security:login.html.twig', $data);
+
+
     }
 
     public function checkAction()
     {
+
         throw new \RuntimeException('You must configure the check path to be handled by the firewall using form_login in your security firewall configuration.');
     }
 
@@ -82,4 +137,51 @@ class SecurityController extends BaseController
     {
         throw new \RuntimeException('You must activate the logout in your security firewall configuration.');
     }
+
+    /**
+     * @param Request $request
+     * @return bool|mixed|string
+     *
+     * Metodo per la verifica della stringa ritornata dal server di autenticazione centralizzato, dopo verifica della carta operatore
+     *
+     *
+     */
+    public function checkSmartCardString(Request $request){
+
+        $uid = (!empty($request->get('uid')))?$request->get('uid'):"";
+
+
+
+        $time = (!empty($request->get('time')))?$request->get('time'):"";
+        $hash = (!empty($request->get('hash')))?$request->get('hash'):"";
+
+     if ($uid == ""||$time==""||$hash=="") return false;
+
+
+        //$time = 1344334037;
+         $timeattuale = time();
+
+         $differenzatempo = $timeattuale - $time;
+
+        if (($differenzatempo/60)>5){
+            // è passato troppo tempo (5 minuti) dall'ultima richiesta con questa stringa
+            return false;
+        }
+
+         //$hash = '841a734135823022f5555e083c2ab3af674f8388';
+        $dacodificare = $uid.$time.'WHR_auth';
+        $codificato = sha1($dacodificare);
+
+        if (sha1($dacodificare) == $hash){
+            // ok autenticazione valida
+           return $uid;
+        }
+        else{
+            // ko autenticazione errata
+            return false;
+        }
+
+
+    }
+
 }
