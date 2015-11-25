18e4e13bd5794db7cffd1674b86045b4c4f40248
FrancescoGalliEstar <francesco01.galli@estar.toscana.it>
2015-11-25 10:15:54 +0100

    Lavori intermedi su back office - 4 (me la ero dimenticata)

---
+++ src/estar/rda/RdaBundle/Form/AreaType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace estar\rda\RdaBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class AreaType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nome')
+            ->add('descrizione')
+            ->add('gruppogestav')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'estar\rda\RdaBundle\Entity\Area'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'estar_rda_rdabundle_area';
+    }
+}
