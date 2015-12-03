162d614ab8630c93c33743ba245249b2f60b0a2c
FrancescoGalliEstar <francesco01.galli@estar.toscana.it>
2015-12-03 17:07:53 +0100

    lavori parziali su gestione utente. Manca l'update dei gruppi utente e il reset della password.

--- src/estar/rda/RdaBundle/Form/ProfileType.php
+++ src/estar/rda/RdaBundle/Form/ProfileType.php
@@ -5,7 +5,7 @@ namespace estar\rda\RdaBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-class RegistrationType extends AbstractType
+class ProfileType extends AbstractType
 {
 
     public function buildForm(FormBuilderInterface $builder, array $options)
@@ -13,15 +13,8 @@ class RegistrationType extends AbstractType
         $builder
             ->add('email', 'email', array('label' => 'Indirizzo Email', 'translation_domain' => 'FOSUserBundle'))
             ->add('username', null, array('label' => 'Username', 'translation_domain' => 'FOSUserBundle'))
-            ->add('plainPassword', 'repeated', array(
-                'type' => 'password',
-                'options' => array('translation_domain' => 'FOSUserBundle'),
-                'first_options' => array('label' => 'Password'),
-                'second_options' => array('label' => 'Conferma Password'),
-                'invalid_message' => 'fos_user.password.mismatch',
-            ))
              ->add('nomecognome',null, array('label' => 'Nome e Cognome'))
-            ->add('codicefiscale',null, array('label' => 'Codice Fiscale'))
+            ->add('codicefiscale',null, array('label' => 'Codice Fiscale ho detto'))
             ->add('idazienda', 'entity', array(
                 'class' => 'estar\rda\RdaBundle\Entity\Azienda',
                 'choice_label' => 'nome',
@@ -38,6 +31,7 @@ class RegistrationType extends AbstractType
         ;
     }
 
+
     public function getParent()
     {
         return 'fos_user_profile';
@@ -47,4 +41,6 @@ class RegistrationType extends AbstractType
     {
         return 'app_user_profile';
     }
+
+
 }
