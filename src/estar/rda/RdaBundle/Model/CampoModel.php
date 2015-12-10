38fe02f3494e4be20b795a7b9ff24b952596c17b
FrancescoGalliEstar <francesco01.galli@estar.toscana.it>
2015-12-10 16:19:59 +0100

    Finiti i lavori su spostasu-giu-inverti. Da testare.

--- src/estar/rda/RdaBundle/Model/CampoModel.php
+++ src/estar/rda/RdaBundle/Model/CampoModel.php
@@ -77,9 +77,8 @@ class CampoModel extends Controller
             $swap = $campo->getOrdinamento();
             $campo->setOrdinamento($campoDaSpostare->getOrdinamento());
             $campoDaSpostare->setOrdinamento($swap);
-        }
-        //Caso più complesso: il campo non ha figli ma il campo sopra in realtà si
-        if (!$campoFiglio && $campoDaSpostareFiglio) {
+        } elseif (!$campoFiglio && $campoDaSpostareFiglio) {
+            //Caso più complesso: il campo non ha figli ma il campo sopra in realtà si
             $ordinamentoCampo = $campo->getOrdinamento();
             $ordinamentoCampoDaSpostare = $campoDaSpostare->getOrdinamento();
             $ordinamentoCampoDaSpostareFiglio = $campoDaSpostareFiglio->getOrdinamento();
@@ -89,9 +88,8 @@ class CampoModel extends Controller
             $campoDaSpostare->setOrdinamento($ordinamentoCampoDaSpostareFiglio);
             //Il figlio del campo da spostare prende l'ordinamento del campo
             $campoDaSpostareFiglio->setObbligatoriovalidazionetecnica($ordinamentoCampo);
-        }
-        //Moccoli & bestemmie: entrambi i campi hanno figli (e le mogli puttane)
-        if ($campoFiglio && $campoDaSpostareFiglio) {
+        } elseif ($campoFiglio && $campoDaSpostareFiglio) {
+            //Moccoli & bestemmie: entrambi i campi hanno figli (e le mogli puttane)
             //Swappo i padri
             $swap = $campo->getOrdinamento();
             $campo->setOrdinamento($campoDaSpostare->getOrdinamento());
@@ -118,30 +116,106 @@ class CampoModel extends Controller
         $maxOrdinamentoCategoria = $this->getMaxOrdinamento($campo->getIdcategoria()->getId());
 
         //Gestione dei casi particolari: se è già l'ultimo campo o se è il penultimo ma ha un figlio, non faccio nulla.
+        if ($ordinamento == $maxOrdinamentoCategoria) return;
+        if ($this->isPadre($campo) && $ordinamento == $maxOrdinamentoCategoria-1) return;
 
+        //Non dovrei mai entrare in questo if ma lo metto per sicurezza. Un figlio non si sposta da sè.
+        if ($this->isFiglio($campo)) return;
 
+        //Il campo da spostare è...
+        $ordinamentoCampoSotto = $campo->getOrdinamento();
+        $ordinamentoCampoSotto = $ordinamentoCampoSotto + 1;
+        $campoDaSpostare = $this->getCampoByOrdinamento($campo->getIdcategoria(), $ordinamentoCampoSotto);
 
+        //Casistiche:
+        //Se il campo non ha figli e il campo sotto non ha figli, semplice swap
+        if (!$this->isPadre($campo) && !$this->isPadre($campoDaSpostare)) {
+            $swap = $campo->getOrdinamento();
+            $campo->setOrdinamento($campoDaSpostare->getOrdinamento());
+            $campoDaSpostare->setOrdinamento($swap);
+
+        } elseif ($this->isPadre($campo) &&!$this->isPadre($campoDaSpostare)) {
+            //Se il campo ha un figlio e il campo sotto no
+            $swap = $campo->getOrdinamento();
+            //il campo sotto prende l'ordinamento del campo
+            $campoDaSpostare->setOrdinamento($campo->getOrdinamento());
+            //Il campo va immediatamente sopra
+            $campo->setOrdinamento($swap + 1);
+            $campoFiglio = $campo->getFiglio();
+            $campoFiglio->setOrdinamento($swap + 2);
+        } elseif (!$this->isPadre($campo) && $this->isPadre($campoDaSpostare)) {
+            //Il campo non ha un figlio ma il campo sotto si
+            $swap = $campo->getOrdinamento();
+            //Il campo sotto prende l'ordinamento del padre
+            $campoDaSpostare->setOrdinamento($swap);
+            //Il figlio del campo sotto va sotto
+            $campoFiglio = $campoDaSpostare->getFiglio();
+            $campoFiglio->setOrdinamento($swap+1);
+            //Il campo prende l'ordinamento +2
+            $campo->setOrdinamento($swap+2);
+
+        } elseif ($this->isPadre($campo) && $this->isPadre($campoDaSpostare)) {
+            //Se entrambi i campi hanno dei figli
+            $swap = $campo->getOrdinamento();
+            //Il campo sotto prende l'ordinamento del campo
+            $campoDaSpostare->setOrdinamento($swap);
+            //Il figlio del campo da spostare va messo sotto
+            $campoFiglioDaSpostare = $campoDaSpostare->getFiglio();
+            $campoFiglioDaSpostare->setOrdinamento($swap+1);
+            //Il campo va immediatamente sotto
+            $campo->setOrdinamento($swap+2);
+            //Il figlio del campo sotto ancora
+            $campoFiglio = $campo->getFiglio();
+            $campoFiglio->setOrdinamento($swap+3);
+        }
+
+        //Finito. Sia lode ai cavalieri Jedi.
+        $this->em->flush();
     }
 
     /** Libera lo spazio per inserire un campo sopra
      *
      * @param $idcampo l'id del campo sopra il quale inserire
+     * @return l'ordinamento del prossimo campo, ossia l'ordinamento orfano.
      */
     public function inserisciSopra($idCampo) {
         $campo = CampoModel::cast($this->em->getRepository('estarRdaBundle:Campo')
             ->find($idCampo));
-
+        $campi = $this->em->getRepository('estarRdaBundle:Campo')->findBy(
+            array('idcategoria' => $campo->getIdcategoria()->getId()),
+            array('ordinamento' => 'ASC')
+        );
+        //Inizio a spostare dall'ordinamento del campo...
+        $ordinamentoPartenza = $campo->getOrdinamento();
+        foreach($campi as $campoDaModificare)
+            if ($campoDaModificare->getOrdinamento() >= $ordinamentoPartenza-1)
+                $campoDaModificare->setOrdinamento(($campoDaModificare->getOrdinamento())+1);
+        $this->em->flush();
+        return $ordinamentoPartenza;
     }
 
     /*
      * Libera lo spazio per inserire un campo sotto
      *
      * @param $idcampo l'id del campo sotto il quale inserire
+     * @return l'ordinamento del campo "vuoto"
      */
     public function inserisciSotto($idCampo) {
         $campo = CampoModel::cast($this->em->getRepository('estarRdaBundle:Campo')
             ->find($idCampo));
-
+        $campi = $this->em->getRepository('estarRdaBundle:Campo')->findBy(
+            array('idcategoria' => $campo->getIdcategoria()->getId()),
+            array('ordinamento' => 'ASC')
+        );
+        //Inizio a spostare dall'ordinamento del campo...
+        $ordinamentoPartenza = $campo->getOrdinamento();
+        //...a meno che non abbia un figlio, nel qual caso inizio da quello dopo.
+        if ($this->isPadre($campo)) $ordinamentoPartenza = $ordinamentoPartenza + 1;
+        foreach($campi as $campoDaModificare)
+            if ($campoDaModificare->getOrdinamento() >= $ordinamentoPartenza)
+                $campoDaModificare->setOrdinamento(($campoDaModificare->getOrdinamento())+1);
+        $this->em->flush();
+        return $ordinamentoPartenza;
     }
 
     /** Ritorna TRUE se il campo è figlio di qualcuno
