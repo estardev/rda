1a006da5ee9c993a37463fd82b51ef00fdf9f0b5
MrZANO <mrzano.spam@gmail.com>
2015-12-10 14:52:40 +0100

    Alberatura per upload
    TODO
    -verificare che nella persistenza venga registrato tutto il path per il giusto download dei file (attualmente errato)

---
+++ src/estar/rda/RdaBundle/Namer/DirectoryNamer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace estar\rda\RdaBundle\Namer;
+
+use Vich\UploaderBundle\Naming\DirectoryNamerInterface;
+use Vich\UploaderBundle\Mapping\PropertyMapping;
+/**
+ * DirectoryNamer class.
+ */
+class DirectoryNamer implements DirectoryNamerInterface
+{
+    public function directoryName($obj, PropertyMapping  $field) {
+        $idRich = $obj->getIdrichiesta();
+        $idDoc = $obj->getIddocumento();
+        return $upload = 'Richiesta_'.$idRich.'_Documento_'.$idDoc;
+    }
+}
---
+++ src/estar/rda/RdaBundle/Namer/Namer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace estar\rda\RdaBundle\Namer;
+
+use Vich\UploaderBundle\Naming\NamerInterface;
+use Vich\UploaderBundle\Mapping\PropertyMapping;
+/**
+ * Namer class.
+ */
+class Namer implements NamerInterface
+{
+    /**
+     * Creates a name for the file being uploaded.
+     *
+     * @param object $obj The object the upload is attached to.
+     * @param string $field The name of the uploadable field to generate a name for.
+     * @return string The file name.
+     */
+    function name($obj, PropertyMapping $field)
+    {
+        $file = $obj->docFile;
+        $extension = $file->guessExtension();
+        $idRich = $obj->getIdrichiesta();
+        $idDoc = $obj->getIddocumento();
+        return 'Richiesta_'.$idRich.'_Documento_'.$idDoc.'.'.$extension;
+    }
+}
