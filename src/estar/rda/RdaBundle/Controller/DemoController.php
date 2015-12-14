5e62870fd9cd358e6373561d21ca35fb39921453
MrZANO <mrzano.spam@gmail.com>
2015-12-14 12:52:04 +0100

    Server per ricevere stati da Sistematica

--- src/estar/rda/RdaBundle/Controller/DemoController.php
+++ src/estar/rda/RdaBundle/Controller/DemoController.php
@@ -32,32 +32,83 @@ class DemoController extends Controller
      * @Soap\Param("idpratica", phpType = "int")
      * @Soap\Param("data", phpType = "dateTime")
      * @Soap\Param("codicemessaggio", phpType = "int")
-     * @Soap\Result(phpType = "string")
+     * @Soap\Result(phpType = "BeSimple\SoapCommon\Type\KeyValue\String[]")
      */
     public function notifyAction($messaggio=null, $idpratica, $data, $codicemessaggio)
     {   $em = $this->getDoctrine()->getManager();
-        $richiesta = $em->getRepository('estarRdaBundle:Iter')->findBy(
-            array('idrichiesta' => $idpratica),
-            array('id' => 'DESC'));
-        $astato= $richiesta[0]->getAstato();
+        $messaggioErrore="OK";
+        $codice="000";  //OK
+       // //$richiesta = $em->getRepository('estarRdaBundle:Iter')->findBy(
+        //    array('idrichiesta' => $idpratica),
+        //    array('id' => 'DESC'));
+        //$astato= $richiesta[0]->getAstato();
 
         $utente = $em->getRepository('estarRdaBundle:Utente')->findBy(
             array('nomecognome' => "Sistematica"));
-        $idutente= $utente[0]->getId();
+        //$idutente= $utente[0]->getId();
+        $avanti=false;
+        $transizione="";
+        switch($codicemessaggio){
+
+            case '010': $transizione="rifiutata_amm_ABS"; $avanti=true; break;
+            case '020': $transizione="rifiutata_tec_ABS"; $avanti=true; break;
+            case '030': $transizione="rifiutata_amm_ABS"; $avanti=true; break;
+            default : $messaggioErrore="NOK - Errore per codice non corretto"; $codice="050"; $avanti=false; break;
+
+        }
+
+        $richiesta = $em->getRepository('estarRdaBundle:Richiesta')->find($idpratica);
+
+        if($avanti){
+             if($richiesta){
+            // Get the factory
+            $factory = $this->get('sm.factory');
+
+            // Get the state machine for this object, and graph called "simple"
+            $articleSM = $factory->get($richiesta, 'rda');
+            //$utente = $this->get('usercheck.notify')->getUtente();
+            $iter= new Iter();
+
+//        TODO recupero ruolo utente
 
 
-        $iter= new Iter();
-        $iter -> setDastato($astato);
-        $iter -> setAstato((string)$codicemessaggio);
-        $iter -> setIdutente($utente[0]);
-        $iter -> setIdrichiesta($idpratica);
-        $iter -> setMotivazione($messaggio);
-        $iter -> setDataora($data);
-        $em->persist($iter);
+            if ($articleSM->can($transizione)) {
+                $iter->setDastato($articleSM->getState());
+                $articleSM->apply($transizione);
+                $iter->setAstato($articleSM->getState());
+                $iter->setIdrichiesta($richiesta);
+                $iter->setIdutente($utente[0]);
+                $iter->setMotivazione($messaggio);
+                $iter->setDataora($data);
+                $em->persist($iter);
+            }
+
+            $em->flush();
+
+        } else{
+            $messaggioErrore ="NOK - Pratica non trovata";
+            $codice="050";
+        }
+        }
 
 
+       // $iter -> setDastato($astato);
+       // $iter -> setAstato((string)$codicemessaggio);
+       // $iter -> setIdutente($utente[0]);
+       // $iter -> setIdrichiesta($idpratica);
+       // $iter -> setMotivazione($messaggio);
+       // $iter -> setDataora($data);
+       // $em->persist($iter);
 
-        return sprintf('CIAO %s!', $astato);
+        $dateTime = new \DateTime();
+        $dateTime->setTimeZone(new \DateTimeZone('Europe/Rome'));
+       $data=  $dateTime->format(\DateTime::W3C);
+
+        return array(
+            'risposta' => $messaggioErrore,
+            'codice' => $codice,
+            'data' =>  $data
+        );
     }
 
     /**
