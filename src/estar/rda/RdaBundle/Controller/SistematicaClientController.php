0b3eb9747ae10bd51f7a0cf2c9b00bdaaeea193d
ilariagiannetti <ilariagiannetti@gmail.com>
2016-01-22 10:18:13 +0100

    upgrade ws sistematica con passaggio parametri al java e chiamata diretta

--- src/estar/rda/RdaBundle/Controller/SistematicaClientController.php
+++ src/estar/rda/RdaBundle/Controller/SistematicaClientController.php
@@ -241,167 +241,15 @@ class SistematicaClientController extends Controller
         $attachmentSetcontenttype1 = $parametri->getAttachmentSetcontenttype1();
         $requestSetinstanceoperation = $parametri->getRequestSetinstanceoperation();
 
-        // generazione file .java
-        $stringa = "
-import java.io.*;
-import java.math.BigInteger;
-import java.text.SimpleDateFormat;
-import java.util.*;
-
-import javax.activation.DataHandler;
-import javax.activation.FileDataSource;
-import javax.xml.soap.MessageFactory;
-import javax.xml.soap.SOAPException;
-import javax.xml.soap.SOAPMessage;
-import javax.xml.ws.BindingProvider;
-import javax.xml.ws.handler.Handler;
-import javax.xml.ws.handler.HandlerResolver;
-import javax.xml.ws.handler.MessageContext;
-import javax.xml.ws.handler.PortInfo;
-import javax.xml.ws.soap.MTOMFeature;
-
-import it.isharedoc.schemas.instance.InstanceMessageCreateRequest;
-import it.isharedoc.schemas.instance.InstanceMessageCreateRequest.*;
-import it.isharedoc.schemas.instance.InstanceMessageCreateRequest.Attachments.Attachment;
-import it.isharedoc.schemas.instance.InstanceMessageCreateRequest.Contacts.Contact;
-import it.isharedoc.schemas.instance.InstanceMessageCreateRequest.Variables.Variable;
-import it.isharedoc.schemas.instance.InstanceMessageCreateResponse;
-import it.isharedoc.schemas.instance.InstancePort;
-import it.isharedoc.schemas.instance.InstancePortService;
-import it.isharedoc.schemas.instance.ObjectFactory;
-
-public class TestClient {
-
-	public static void main(String[] args) throws SOAPException {
-		InstancePortService service = new InstancePortService();
-
-		service.setHandlerResolver( new HandlerResolver() {
-
-			@Override
-			public List<Handler> getHandlerChain(PortInfo portInfo) {
-				List<Handler> handlerList = new ArrayList<Handler>();
-                handlerList.add(new WSSUsernameTokenSecurityHandler(\"" . $user . "\", \"" . $psw . "\"));
-                return handlerList;
-			}
-		});
-
-		InstancePort port = service.getInstancePortSoap11( new MTOMFeature(10240));
-		Map<String, Object> ctxt = ((BindingProvider)port).getRequestContext();
-		ctxt.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, \"" . $wsdl . "\");
-
-		String fileName = \"" . $path . ".zip\";
-		String filePath = \"../../" . $directory_sender . "/" . $path . "/\" + fileName;
-
-		InstanceMessageCreateRequest request = prepareRequestObject(filePath);
-
-		InstanceMessageCreateResponse response = port.instanceMessageCreate(request);
-		System.out.println( response );
-		BufferedWriter output = null;
-		try {
-            File file = new File(\"" . $path . "_protocollo.txt\");
-			output = new BufferedWriter(new FileWriter(file));
-			output.write(response.getIdentifier().getValue());
-		} catch ( IOException e ) {
-        e.printStackTrace();
-    } finally {
-        try {
-            if ( output !=  null ) output.close();
-        } catch ( Exception ex ) {
-
-        }
-
-		}
-	}
-
-private static InstanceMessageCreateRequest prepareRequestObject(String filePath) {
-SimpleDateFormat sdf = new SimpleDateFormat( \"dd/MM/yyyy HH:mm:ss.SSS\");
-ObjectFactory factory = new ObjectFactory();
-
-InstanceMessageCreateRequest request = new InstanceMessageCreateRequest();
-request.setPartitionId( BigInteger.valueOf(5));
-request.setStoryboardCode( \"" . $storyboardcode . "\" );
-request.setMessageBoxId( BigInteger.valueOf(4));
-request.setMetaViewName( factory.createInstanceMessageCreateRequestMetaViewName(\"" . $setmetaviewname . "\"));
-request.setDirection(\"" . $setdirection . "\");
-
-Contacts contacts = factory.createInstanceMessageCreateRequestContacts();
-Contact contact = factory.createInstanceMessageCreateRequestContactsContact();
-contact.setType(\"" . $contactSettype1 . "\");
-contact.setReferenceType(\"" . $contactReferencetype1 . "\");
-contact.setReferenceCode(\"" . $contactReferencecode1 . "\");
-contacts.getContact().add(contact);
-
-contact = factory.createInstanceMessageCreateRequestContactsContact();
-contact.setType(\"" . $contactSettype2 . "\");
-contact.setReferenceType(\"" . $contactReferencetype2 . "\");
-contact.setReferenceCode(\"" . $contactReferencecode2 . "\");
-contacts.getContact().add(contact);
-
-contact = factory.createInstanceMessageCreateRequestContactsContact();
-contact.setType(\"" . $contactSettype3 . "\");
-contact.setReferenceType(\"" . $contactReferencetype3 . "\");
-contact.setReferenceCode(\"" . $contactReferencecode3 . "\");
-contacts.getContact().add(contact);
-
-request.setContacts( factory.createInstanceMessageCreateRequestContacts(contacts));
-
-request.setSubject( factory.createInstanceMessageCreateRequestSubject(\"Richiesta del \" + sdf.format(new Date())));
-
-request.setTags( factory.createInstanceMessageCreateRequestTags( \"" . $idRichiesta . "\" ));
-
-Variables variables = factory.createInstanceMessageCreateRequestVariables();
-Variable variable = factory.createInstanceMessageCreateRequestVariablesVariable();
-variable.setKey( \"" . $variableSetkey1 . "\" );
-variable.setType( \"" . $variableSettype1 . "\" );
-variable.setValueString( factory.createInstanceMessageCreateRequestVariablesVariableValueString( \"" . $variableSetvaluestring1 . "\" ));
-variables.getVariable().add(variable);
-
-request.setVariables( factory.createInstanceMessageCreateRequestVariables(variables) );
-
-File f = new File( filePath );
-
-Attachments attachments = factory.createInstanceMessageCreateRequestAttachments();
-Attachment attachment = factory.createInstanceMessageCreateRequestAttachmentsAttachment();
-attachment.setFileset( \"" . $attachmentSetfileset1 . "\" );
-attachment.setFilename( f.getName() );
-attachment.setContentType( factory.createInstanceMessageCreateRequestAttachmentsAttachmentContentType( \"" . $attachmentSetcontenttype1 . "\" ));
-
-FileDataSource fds = new FileDataSource( f );
-DataHandler handler = new DataHandler( fds );
-
-attachment.setData( handler );
-
-attachments.getAttachment().add(attachment);
-
-request.setAttachments( factory.createInstanceMessageCreateRequestAttachments(attachments));
-
-request.setStartWorkflow( factory.createInstanceMessageCreateRequestStartWorkflow(true) );
-request.setInstanceOperation( factory.createInstanceMessageCreateRequestInstanceOperation( \"" . $requestSetinstanceoperation . "\" ));
-
-return request;
-}
-
-}
-
-";
-        file_put_contents("client/src/TestClient.java", $stringa, FILE_USE_INCLUDE_PATH);
-
-        // generazione file batch
-        $righe_batch = "@echo off \ncd client\\src\\ \n";
-        $righe_batch .= "javac TestClient.java \n";
-        $righe_batch .= "java TestClient \n";
-        $righe_batch .= "cd ..\\..\\ \n";
-        $righe_batch .= "pause";
-        $batfile = $path . "_esec.bat";
-
-        file_put_contents($batfile, $righe_batch);
-
-        // esecuzione file batch
+        // esecuzione file java
         // abilitare su php.ini di apache (wamp) safe_mode_exec_dir=off
-        exec($path . "_esec.bat", $output, $return);
+        //exec($path . "_esec.bat", $output, $return);
+        exec("java -cp ./client/src TestClient ".$path." ../../sender/".$path."/", $output, $return);
+
 
         // estrazione del numero di protocollo dalla risposta
-        $numProt = file_get_contents("/client/src/" . $path . "_protocollo.txt", FILE_USE_INCLUDE_PATH);
+        //$numProt = file_get_contents("/client/src/" . $path . "_protocollo.txt", FILE_USE_INCLUDE_PATH);
+        $numProt = file_get_contents($path . "_protocollo.txt", FILE_USE_INCLUDE_PATH);
 
         // scrittura del numero di protocollo sulla richiesta
         $em = $this->getDoctrine()->getManager();
@@ -422,8 +270,9 @@ return request;
         // cancellazione file
         // da decommentare nel caso non volessimo più fare storage delle richieste zippate e inviate
         //$this->delTree($path);
-        unlink($batfile);
-        unlink("client/src/" . $path . "_protocollo.txt");
+        //unlink($batfile);
+        //unlink("client/src/" . $path . "_protocollo.txt");
+        unlink($path . "_protocollo.txt");
 
         return $this->redirect($this->generateUrl("richiesta"));
     }
