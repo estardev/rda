4031830a7cf5f95279043a7ed34cdd406f61858d
FrancescoGalliEstar <cinghialemannaro@gmail.com>
2016-03-21 16:37:31 +0100

    Test gestione GUID

--- src/estar/rda/RdaBundle/Model/ClientSistematica.php
+++ src/estar/rda/RdaBundle/Model/ClientSistematica.php
@@ -383,22 +383,14 @@ class ClientSistematica
 
     }
 
-    function getGUID(){
-        if (function_exists('com_create_guid')){
-            return com_create_guid();
-        }
-        else {
-            mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
-            $charid = strtoupper(md5(uniqid(rand(), true)));
-            $hyphen = chr(45);// "-"
-            $uuid = chr(123)// "{"
-                .substr($charid, 0, 8).$hyphen
-                .substr($charid, 8, 4).$hyphen
-                .substr($charid,12, 4).$hyphen
-                .substr($charid,16, 4).$hyphen
-                .substr($charid,20,12)
-                .chr(125);// "}"
-            return $uuid;
+    function getGUID()
+    {
+        {
+            if (function_exists('com_create_guid') === true) {
+                return trim(com_create_guid(), '{}');
+            }
+
+            return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
         }
     }
 }
