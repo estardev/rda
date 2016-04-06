2f0bdf48f750cbf8a7469ca0dcdedf22e776d16c
MrZANO <mrzano.spam@gmail.com>
2016-04-06 14:12:07 +0200

    inserimento prima riga in iter e idUtente in Richiesta

--- src/estar/rda/RdaBundle/Controller/HomePageController.php
+++ src/estar/rda/RdaBundle/Controller/HomePageController.php
@@ -35,38 +35,52 @@ class HomePageController extends Controller
         $richiesta = $em->getRepository('estarRdaBundle:Richiesta')->findAll();
         $categoria = $em->getRepository('estarRdaBundle:Categoria')->findAll();
 
-        $nValtec=$em->createQuery("SELECT COUNT(r) FROM estarRdaBundle:Richiesta r WHERE r.idutente=$utenteSessione AND r.status='attesa_val_tec'")->getSingleScalarResult();
+        $nValtec=$em->createQuery("SELECT COUNT(r) FROM estarRdaBundle:Richiesta r WHERE r.status='attesa_val_tec' ")->getSingleScalarResult();
 
         $nBozza=$em->createQuery("SELECT COUNT(r) FROM estarRdaBundle:Richiesta r WHERE r.idutente=$utenteSessione AND r.status='bozza'")->getSingleScalarResult();
 
-        $nValAmm=$em->createQuery("SELECT COUNT(r) FROM estarRdaBundle:Richiesta r WHERE r.idutente=$utenteSessione AND r.status='attesa_val_amm'")->getSingleScalarResult();
+        $nValAmm=$em->createQuery("SELECT COUNT(r) FROM estarRdaBundle:Richiesta r WHERE r.status='attesa_val_amm'")->getSingleScalarResult();
 
-        $nDainv=$em->createQuery("SELECT COUNT(r) FROM estarRdaBundle:Richiesta r WHERE r.idutente=$utenteSessione AND r.status='da_inviare_ABS'")->getSingleScalarResult();
-
-
-
+        $nDainv=$em->createQuery("SELECT COUNT(r) FROM estarRdaBundle:Richiesta r WHERE r.status='da_inviare_ABS'")->getSingleScalarResult();
 
         //David_20151029: Aggiunto il redirect sulla lista richieste per categoria impostata in sessione
+        //modificato da Demetrio_20160406:
+        // se ci sono operazioni da fare torna alla homepage di riepilogo,
+        // se non ci sono operazioni da fare ed è settata la categoria torna nella categoria
 
         $idCategoria = $this->get('session')->get('homepageSelectCategoria');
 
-        if (!empty($idCategoria)){
+        if(!empty($nValAmm) or !empty($nDainv) or !empty($nBozza) or !empty($nValtec)){
+            return $this->render('estarRdaBundle:HomePage:index.html.twig', array(
+                'richiesta' => $richiesta,
+                'categoria'=> $categoria,
+                'utente' => ucfirst($utenteSessione),
+                'nbozza' => $nBozza,
+                'nvaltec' => $nValtec,
+                'nvalamm' => $nValAmm,
+                'ndainvABS' => $nDainv,
+
+            ));
+        }
+        else if (!empty($idCategoria)){
             return $this->redirect($this->generateUrl('richiesta_bycategoria',array('idCategoria'=>$idCategoria)));
         }
-        else
+        else{
+            return $this->render('estarRdaBundle:HomePage:index.html.twig', array(
+                'richiesta' => $richiesta,
+                'categoria'=> $categoria,
+                'utente' => ucfirst($utenteSessione),
+                'nbozza' => $nBozza,
+                'nvaltec' => $nValtec,
+                'nvalamm' => $nValAmm,
+                'ndainvABS' => $nDainv,
+
+            ));
+        }
 
         // fine:David_20151029
 
-        return $this->render('estarRdaBundle:HomePage:index.html.twig', array(
-            'richiesta' => $richiesta,
-            'categoria'=> $categoria,
-            'utente' => ucfirst($utenteSessione),
-            'nbozza' => $nBozza,
-            'nvaltec' => $nValtec,
-            'nvalamm' => $nValAmm,
-            'ndainvABS' => $nDainv,
 
-        ));
 
     }
 
