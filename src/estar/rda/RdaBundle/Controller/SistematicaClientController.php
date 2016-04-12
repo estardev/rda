5594fa70257ecc261d9403928d85c01f42308c88
MrZANO <mrzano.spam@gmail.com>
2016-04-12 15:54:26 +0200

    inserimento prima riga in iter e idUtente in Richiesta

--- src/estar/rda/RdaBundle/Controller/SistematicaClientController.php
+++ src/estar/rda/RdaBundle/Controller/SistematicaClientController.php
@@ -105,13 +105,33 @@ class SistematicaClientController extends Controller
         return $num = $max + 1;
     }
 
-    public function generateZip($idCategoria, $idRichiesta)
+    public function generateZip($idCategoria, $idRichiesta, $tipologia)
     {
-        // generazione file pdf e zip
         $directory_sender = "sender";
         $num = $this->num();
         $path = $num . "_Richiesta_" . $idRichiesta . "_categoria_" . $idCategoria;
         if (!is_dir($directory_sender . "/" . $path)) mkdir($directory_sender . "/" . $path, 0777);
+
+
+        switch ($tipologia) {
+
+            case "Nuova":
+
+                break;
+
+            case "Documentazione Aggiuntiva":
+
+                break;
+
+            case "Documentazione Richiesta da RUP":
+
+                break;
+
+        }
+
+
+        // generazione file pdf e zip
+
 
         $em = $this->getDoctrine()->getManager();
         $query = $em->createQuery('SELECT IDENTITY (c.iddocumento ) as iddocu
@@ -239,6 +259,7 @@ class SistematicaClientController extends Controller
 
         $this->get('knp_snappy.pdf')->generateFromHtml($html, $directory_sender . "/" . $path . "/" . $num . "_Richiesta_" . $idRichiesta . ".pdf");
 
+        //TODO: ELIMINARE ZIP
         $zip = \Comodojo\Zip\Zip::create($directory_sender . '/' . $path . '/' . $path . '.zip');
         $zip->add($directory_sender . "/" . $path, true); //->add($pathdocumenti, true);
 
@@ -248,16 +269,18 @@ class SistematicaClientController extends Controller
         $documenti = $em->getRepository('estarRdaBundle:Richiestadocumento')->findBy(array('idrichiesta' => $idRichiesta));
         foreach ($documenti as $documentidazippare) {
             if (is_null($documentidazippare->getNumeroprotocollo()))
+                //TODO: CAMBIARE LO ZIP CON LO SPOSTAMENTO DEI FILE NEL PATH DELLA CARTELLA DA INVIARE
                 $zip->setPath($pathdocumenti)->add($documentidazippare->getFilepath());
         }
 
         $documentiliberi = $em->getRepository('estarRdaBundle:Richiestadocumentolibero')->findBy(array('idrichiesta' => $idRichiesta));
         foreach ($documentiliberi as $documentiliberidazippare) {
             if (is_null($documentiliberidazippare->getNumeroprotocollo()))
+                //TODO: CAMBIARE LO ZIP CON LO SPOSTAMENTO DEI FILE NEL PATH DELLA CARTELLA DA INVIARE
                 $zip->setPath($pathdocumenti)->add($documentiliberidazippare->getFilepath());
         }
 
-
+        //TODO: ELIMINARE ZIP
         $zip->close();
 
         return array('esito' => true, 'progressivo' => $num, 'path' => $path);
@@ -300,7 +323,7 @@ class SistematicaClientController extends Controller
                 break;
 
             case "Nuova":
-                $ritorno = $this->generateZip($idCategoria, $idRichiesta);
+                $ritorno = $this->generateZip($idCategoria, $idRichiesta, $tipologia);
                 if ($ritorno['esito']) {
                     $idGestav = "xxxxx";
                     $idgara="";
@@ -311,7 +334,7 @@ class SistematicaClientController extends Controller
                 break;
 
             case "Documentazione Aggiuntiva":
-                $ritorno = $this->generateZip($idCategoria, $idRichiesta);
+                $ritorno = $this->generateZip($idCategoria, $idRichiesta, $tipologia);
                 if ($ritorno['esito']) {
                     $idGestav = $richiesta->getIdgestav();
                     $idgara="";
@@ -322,7 +345,7 @@ class SistematicaClientController extends Controller
                 break;
 
             case "Documentazione Richiesta da RUP":
-                $ritorno = $this->generateZip($idCategoria, $idRichiesta);
+                $ritorno = $this->generateZip($idCategoria, $idRichiesta, $tipologia);
                 if ($ritorno['esito']) {
                     $idGestav = $richiesta->getIdgestav();
                     $idgara= $richiesta->getCodicegara();
