9d972b199e3f3a9d94fefa446f313b131c40147d
gianlu <licarigianluca@gmail.com>
2016-04-14 12:28:01 +0200

    modificato form builder service

--- src/estar/rda/RdaBundle/Controller/FormTemplateController.php
+++ src/estar/rda/RdaBundle/Controller/FormTemplateController.php
@@ -565,68 +565,12 @@ class FormTemplateController extends Controller
 
 
         $em = $this->getDoctrine()->getManager();
-        $usercheck = $this->get("usercheck.notify");
-        $diritti = $usercheck->allRole($idCategoria);
-        //FG il controllo dei campi è intenzionalmente tenuto fuori da ACL
-        $query = $em->createQuery('SELECT c.id AS idcampo, identity (c.idcategoria) as pippocategoria, c.nome,c.descrizione,c.fieldset,c.tipo,c.dataattivazione,vc.id,vc.valore
-                                    FROM estarRdaBundle:Campo c LEFT JOIN estarRdaBundle:Valorizzazionecamporichiesta vc
-                                    WITH c.id = vc.idcampo
-                                    AND vc.idrichiesta = :idRichiesta')
-            ->setparameter('idRichiesta', $idRichiesta);
-        //FG20160317 hack: non c'è verso di far capire a doctrine che voglio solo quelli di una categoria.
-        $campiValorizzatiIntermedi = $query->getResult();
-        //Itero sul risultato e sego. Mi vergogno di me stesso.
-        $campiValorizzati = array();
-        foreach ($campiValorizzatiIntermedi as $campovalorizzato) {
-            $campoTemp = $campovalorizzato;
-            if ($campoTemp['pippocategoria'] == $idCategoria)
-                array_push($campiValorizzati, $campoTemp);
-        }
 
-
-        $formbuilder = $this->createFormBuilder();
-//        $fieldsetVisitati = array();
         $richiesta = $em->getRepository('estarRdaBundle:Richiesta')->find($idRichiesta);
-        $formbuilder->add("titolo", "text", array(
-            'label' => "titolo",
-            'data' => $richiesta->getTitolo(),
-            'read_only' => true
-        ));
-        $formbuilder->add("descrizione", "textarea", array(
-            'label' => "descrizione",
-            'data' => $richiesta->getDescrizione(),
-            'read_only' => true
-        ));
-        foreach ($campiValorizzati as $campovalorizzato) {
-//            $campo = $campovalorizzato->getIdcampo();
-            $campo = $campovalorizzato;
+        $res = $this->get('form_template_factory')->build($this->get('form.factory')->createNamedBuilder('form', 'form', array()), $richiesta, 2);
+        $form = $res[0];
 
-            $repository = $this->getDoctrine()->getRepository('estarRdaBundle:Campo');
-            $campoCheck = $repository->find($campo['idcampo']);
-            if (!($diritti->campoVisualizzabile($diritti, $campoCheck))) continue;
 
-//            if ($campo->getTipo() == 'choice') {
-            if ($campo['tipo'] == 'choice') {
-//                $fieldsetName = $campo->getFieldset();
-                $descrizioneValore = $this->selectedOption($this->getChoicesOptions($campoCheck->getFieldset()), $campo['valore']);
-                $formbuilder->add($campo['nome'] . '-' . $campo['id'], 'text', array(
-                    'label' => $campo['descrizione'],
-                    'data' => $descrizioneValore,
-                    'read_only' => true
-                ));
-            } else {
-
-                $formbuilder->add($campo['nome'] . '-' . $campo['id'], $campo['tipo'], array(
-                    'label' => $campo['descrizione'],
-                    'data' => $campo['valore'],
-                    'read_only' => true
-                ));
-            }
-        }
-        $form = $formbuilder->getForm();
-//        if (!$entity) {
-//            throw $this->createNotFoundException('Unable to find FormTemplate entity.');
-//        }
 
         $html = $this->renderView('::printbase.html.twig', array(
 //            'entity' => $entity,
