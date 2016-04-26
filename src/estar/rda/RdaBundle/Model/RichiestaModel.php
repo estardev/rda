d0633c016438f15c461694b94afef44ef17f956c
MrZANO <mrzano.spam@gmail.com>
2016-04-26 14:35:37 +0200

    inserimento prima riga in iter e idUtente in Richiesta

--- src/estar/rda/RdaBundle/Model/RichiestaModel.php
+++ src/estar/rda/RdaBundle/Model/RichiestaModel.php
@@ -807,7 +807,7 @@ class RichiestaModel
 
             case '110':
                 //Annullato ABS
-                if ($richiesta->getStatus() == RichiestaModel::STATUS_INVIATA_ABS or $articleSM->can('annullamento_ABS') or $iter->getAstatogestav()==RichiestaModel::STATUSABS_ANNULLATA) {
+                if ($richiesta->getStatus() == RichiestaModel::STATUS_INVIATA_ABS or $articleSM->can('annullamento_ABS') or $richiesta->getStatus()==RichiestaModel::STATUS_ANNULLATA) {
                     $iter= new Iter();
                     $iter->setDastato($articleSM->getState());
                     //$articleSM->apply('annullamento_ABS');
