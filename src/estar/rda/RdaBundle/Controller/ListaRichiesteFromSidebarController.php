4d2d6c81ce8fc76d87ff0f8bbb588627bf098a5c
MrZANO <mrzano.spam@gmail.com>
2016-05-09 17:52:07 +0200

    null pratica

--- src/estar/rda/RdaBundle/Controller/ListaRichiesteFromSidebarController.php
+++ src/estar/rda/RdaBundle/Controller/ListaRichiesteFromSidebarController.php
@@ -20,14 +20,22 @@ class ListaRichiesteFromSidebarController extends Controller
                 $query = $em->createQuery(
                     'SELECT r
                         FROM estarRdaBundle:Richiesta r
-                        WHERE r.status LIKE ?1 OR r.status LIKE ?2 OR r.status LIKE ?3'
-                    )->setParameters(array(1 => 'bozza', 2 => 'attesa_val_tec', 3 => 'attesa_val_amm'));
+                        WHERE r.status LIKE ?1 OR r.status LIKE ?2 OR r.status LIKE ?3 OR r.status LIKE ?4 OR r.status LIKE ?5'
+                    )->setParameters(array(1 => 'bozza', 2 => 'attesa_val_tec', 3 => 'attesa_val_amm', 4 => 'attesa_val_amm', 5 => 'da_inviare_ABS'));
 
                 $entities = $query->getResult();
                 break;
             case 'lavorate_1':
-                $criteria = array('status' => 'da_inviare_ABS');
-                $entities = $em->getRepository('estarRdaBundle:Richiesta')->findBy($criteria);
+                //$criteria = array('status' => 'da_inviare_ABS');
+                //$entities = $em->getRepository('estarRdaBundle:Richiesta')->findBy($criteria);
+                $query = $em->createQuery(
+                    'SELECT r
+                        FROM estarRdaBundle:Richiesta r
+                        WHERE r.status LIKE ?1 OR r.status LIKE ?2 OR r.status LIKE ?3 OR r.status LIKE ?4 OR r.status LIKE ?5'
+                )->setParameters(array(1 => 'inviata_ABS', 2 => 'evasa_ABS', 3 => 'rigetto_ABS', 4 => 'chiusa_ABS', 5 => 'annullata_ABS'));
+
+                $entities = $query->getResult();
+
                 break;
         }
 
