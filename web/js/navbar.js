bd3d4225d92cfdf3628d61875dea7e754d73f573
MrZANO <mrzano.spam@gmail.com>
2017-04-19 14:47:43 +0200

    modifica printbase.html.twig

--- web/js/navbar.js
+++ web/js/navbar.js
@@ -87,7 +87,7 @@ $(document).ready(function()
     $(document).on("click", "#allview", function () {
         $('#homepageSelectBtnCategoria').prop('disabled',true);
         $('#homepageMostraTutte').prop('disabled',true);
-
+        $('#homepageSelectCategoria').val('defaultDisabledNavbarOption');
         //alert("Bottone: ");
         var all = "all";
         window.location.replace(Routing.generate('richiesta_viewall', {'all': all}));
